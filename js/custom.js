94fcc3187c31bb163f460ca5f642f43f467cf403
Gethin1990 <Gethin1990@users.noreply.github.com>
2021-09-27 08:49:41 +0000

    Merge pull request #70 from Gethin1990/development
    Development Gethin1990/hugoblog@c6203714f0e61257287a9c2912e86ba3c5c967c8

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,58 @@
-$(".dropdown").each(function() {
-    $(this).on("click", function(e) {
+$(".dropdown").each(function () {
+    $(this).on("click", function (e) {
         // 收起其他菜单
         if (isMobile()) {
-            $(".menu").find(".dropdown-menu").not($(this).children("div")).slideUp("fast");    
+            $(".menu").find(".dropdown-menu").not($(this).children("div")).slideUp("fast");
         } else {
-            $(".menu-inner").find(".dropdown-menu").not($(this).children("div")).slideUp("fast");    
+            $(".menu-inner").find(".dropdown-menu").not($(this).children("div")).slideUp("fast");
         }
-        e.stopPropagation(); 
+        e.stopPropagation();
         var downmenu = $(this).children("div");
         // 展开菜单
         downmenu.slideToggle("fast");
         // 点击其他地方，隐藏菜单
         if (downmenu.is(":visible")) {
-            $(document).one("click", function() {   
+            $(document).one("click", function () {
                 downmenu.slideUp("fast");
             });
         }
     });
 });
-function isMobile(){
+function isMobile() {
     return window.matchMedia("only screen and (max-width: 680px)").matches;
 }
-/* 轮播背景图片 */
-$(function () {
-	$.backstretch([
-		  "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/ink1-2021-05-17-20-04-31.gif",
-          "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/ink2-2021-05-17-20-27-18.gif",
-          "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/ink3-2021-05-17-20-29-18.gif"
-	], { duration: 60000, fade: 1500 });
+/* 主题背景图 */
+$(".theme-switch").each(function () {
+    $(this).on("click", function (e) {
+        if (window.localStorage && localStorage.getItem('theme')) {
+            // light
+            if (localStorage.getItem('theme') === 'dark') {
+                $.backstretch([
+                    "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/ink1-2021-05-17-20-04-31.gif"])
+            } else {
+                //dark
+                $.backstretch([
+                    "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/black.jpg"])
+            }
+
+        }
+    })
 });
 
-$(document).ready(function(){
-	if ($('#rewardButton')[0]) {
-	    $("#rewardButton").on("click", function() {
-	       $('#QR').slideToggle(); 
-	    });
-	}
+$(document).ready(function () {
+    if ($('#rewardButton')[0]) {
+        $("#rewardButton").on("click", function () {
+            $('#QR').slideToggle();
+        });
+    }
+    $.backstretch([
+        "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/ink1-2021-05-17-20-04-31.gif"])
+
+    if (window.localStorage && localStorage.getItem('theme')) {
+        if (localStorage.getItem('theme') === 'dark') {
+            $.backstretch([
+                "https://cdn.jsdelivr.net/gh/Gethin1990/PicBed/BlogImg/black.jpg"])
+        }
+
+    }
 });
